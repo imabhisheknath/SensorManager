265bbb1fd4f4f975369908b733d78d5d34443ad4
Abhishek <imabhishek.nath@gmail.com>
2018-02-07 12:42:16 +0530

    Initial commit

---
+++ app/src/main/java/com/hoyo/sensormanager/KEY.java
@@ -0,0 +1,36 @@
+package com.hoyo.sensormanager;
+
+/**
+ * Created by Administrator on 01-02-2018.
+ */
+
+public class KEY {
+
+    //Acc
+    public static String ACC_X_VALUE = "acc_x_value";
+    public static String ACC_Y_VALUE = "acc_y_value";
+    public static String ACC_Z_VALUE = "acc_z_value";
+
+
+    //Roation
+    public static String AZIMUTH_ORIATION_ANGLE = "azimuth_oriatation_angle";
+    public static String PITCH_ORIATION_ANGLE = "pitch_oriatation_angle";
+    public static String ROLL_ORIATION_ANGLE = "roll_oriatation_angle";
+
+
+    //Heart Rate
+    public static String HEART_RATE = "heart_rate";
+
+
+    //GyroScope
+    public static String ROTATION_X = "rotation_x";
+    public static String ROTATION_Y = "rotation_y";
+    public static String ROTATION_Z = "rotation_z";
+
+
+    //pedometer
+
+    public static String PEDOMETER = "pedometer";
+    public static String REGISTER_ERROR = "error";
+
+}
---
+++ app/src/main/java/com/hoyo/sensormanager/SensorsManager.java
@@ -0,0 +1,261 @@
+package com.hoyo.sensormanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 01-02-2018.
+ */
+
+public abstract class SensorsManager {
+    private SensorManager mSensorManager;
+    Context context;
+
+    public enum TYPE {
+        ACC, ORI, GYRO, HEART_RATE, PEDOMETER
+    }
+
+
+    public SensorsManager(Context context) {
+
+        this.context = context;
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+
+
+    }
+
+
+    abstract void Accelerometer(ContentValues contentValues);
+
+    abstract void OnError(ContentValues contentValues);
+
+    abstract void GyroScopeSensor(ContentValues contentValues);
+
+    abstract void HeartBeatSensor(ContentValues contentValues);
+
+    abstract void OriantationSensor(ContentValues contentValues);
+
+    abstract void PedometerSensor(ContentValues contentValues);
+
+
+    // Handling the Listners
+
+    public void setListner(TYPE... type) {
+        for (int i = 0; i < type.length; i++) {
+            switch (type[i]) {
+                case ACC:
+                    Log.d("mylog", "Alog");
+                    Sensor mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                    if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+                        mSensorManager.registerListener(accelometerListner, mAccelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    } else {
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(KEY.REGISTER_ERROR, "we dont have an accelerometer!");
+                        OnError(contentValues);
+
+                    }
+
+                    break;
+
+                case GYRO:
+                    Sensor mGyroScopeSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+                    if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
+                        mSensorManager.registerListener(gyroscopeSensorListner, mGyroScopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    } else {
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(KEY.REGISTER_ERROR, "we dont have  gyroscope!");
+                        OnError(contentValues);
+                    }
+
+                    break;
+
+                case HEART_RATE:
+                    if (mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE) != null) {
+                        Sensor mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+                        mSensorManager.registerListener(heartratesensorlistner, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    } else {
+
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(KEY.REGISTER_ERROR, "we dont have  heartratesensor!");
+                        OnError(contentValues);
+                    }
+
+                    break;
+
+
+                case ORI:
+                    if (mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION) != null) {
+
+                        Sensor mOriatationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+                        mSensorManager.registerListener(oriantationsensorlistner, mOriatationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    } else {
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(KEY.REGISTER_ERROR, "we dont have an OriationSensor!");
+                        OnError(contentValues);
+                    }
+                    break;
+
+
+                case PEDOMETER:
+                    if (mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+
+                        Sensor mPedometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+                        mSensorManager.registerListener(pedometerlistner, mPedometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    } else {
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(KEY.REGISTER_ERROR, "we dont have Pedometersensor!");
+                        OnError(contentValues);
+                    }
+                    break;
+
+            }
+
+
+        }
+
+    }
+
+
+    //unregister Sensors
+
+    public void unregisterSensors(TYPE... types) {
+
+
+        for (int i = 0; i < types.length; i++) {
+            switch (types[i]) {
+                case ACC:
+                    mSensorManager.unregisterListener(accelometerListner);
+                    break;
+                case ORI:
+                    mSensorManager.unregisterListener(oriantationsensorlistner);
+                    break;
+                case HEART_RATE:
+                    mSensorManager.unregisterListener(heartratesensorlistner);
+                    break;
+                case GYRO:
+                    mSensorManager.unregisterListener(gyroscopeSensorListner);
+                    break;
+                case PEDOMETER:
+                    mSensorManager.unregisterListener(pedometerlistner);
+                    break;
+
+
+            }
+        }
+
+    }
+
+
+    //AccMeter Sensor
+    private SensorEventListener accelometerListner = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            ContentValues contentValues = new ContentValues();
+            float[] values = sensorEvent.values;
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+            contentValues.put(KEY.ACC_X_VALUE, x);
+            contentValues.put(KEY.ACC_Y_VALUE, y);
+            contentValues.put(KEY.ACC_Z_VALUE, z);
+            Accelerometer(contentValues);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+
+    //Gyroscope Sensor
+    private SensorEventListener gyroscopeSensorListner = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(KEY.ROTATION_X, x);
+            contentValues.put(KEY.ROTATION_Y, y);
+            contentValues.put(KEY.ROTATION_Z, z);
+            GyroScopeSensor(contentValues);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+
+    //HeartRateSensor
+    private SensorEventListener heartratesensorlistner = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            int a = (int) sensorEvent.values[0];
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(KEY.HEART_RATE, a);
+            HeartBeatSensor(contentValues);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+
+    //Oriantation Sensor
+    private SensorEventListener oriantationsensorlistner = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            ContentValues contentValues = new ContentValues();
+            float azimuth = sensorEvent.values[0];
+            float pitch = sensorEvent.values[1];
+            float roll = sensorEvent.values[2];
+            contentValues.put(KEY.AZIMUTH_ORIATION_ANGLE, azimuth);
+            contentValues.put(KEY.PITCH_ORIATION_ANGLE, pitch);
+            contentValues.put(KEY.ROLL_ORIATION_ANGLE, roll);
+            OriantationSensor(contentValues);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+
+    //pedometer sensors
+    private SensorEventListener pedometerlistner = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(KEY.PEDOMETER, String.valueOf(sensorEvent.values[0]));
+            PedometerSensor(contentValues);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+
+}
+
+
+
+
+
+
+
+
+
+
