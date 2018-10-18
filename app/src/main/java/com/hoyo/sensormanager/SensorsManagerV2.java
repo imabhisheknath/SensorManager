1130f6c3975f3755d8a34196c0cd405b4755768e
Praba <prabalinux@gmail.com>
2018-10-18 16:09:05 +0530

    Updated Code

--- app/src/main/java/com/hoyo/sensormanager/SensorsManagerV2.java
+++ app/src/main/java/com/hoyo/sensormanager/SensorsManagerV2.java
@@ -273,7 +273,6 @@ public class SensorsManagerV2 {
         }
     };
 
-
     //HeartRateSensor
     private SensorEventListener heartrateSensorListener = new SensorEventListener() {
         @Override
